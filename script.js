70f6fa7f124d4de5009fc9ab414ca08162136790
Phalyaj5119 <123476852+Phalyaj5119@users.noreply.github.com>
2024-02-13 13:12:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const lightboxImage = document.getElementById('lightboxImage');
+const thumbnailGallery = document.getElementById('thumbnailGallery');
+const images = ['rectangle-27@2x.png', 'rectangle-25@2x.png', 'img1.png', 'img2.png', 'rectangle-26@2x.png']; // Image URLs
+let currentIndex = 0;
+
+function displayImage(index) {
+  lightboxImage.style.backgroundImage = `url(${images[index]})`;
+}
+
+function generateThumbnails() {
+  thumbnailGallery.innerHTML = '';
+  images.forEach((image, index) => {
+    const thumbnail = document.createElement('div');
+    thumbnail.classList.add('thumbnail');
+    thumbnail.style.backgroundImage = `url(${image})`;
+    thumbnail.addEventListener('click', () => {
+      currentIndex = index;
+      displayImage(currentIndex);
+    });
+    thumbnailGallery.appendChild(thumbnail);
+  });
+}
+
+function startSlideshow() {
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    displayImage(currentIndex);
+  }, 5000); // Change slideshow interval as needed (in milliseconds)
+}
+
+generateThumbnails();
+displayImage(currentIndex);
+startSlideshow();
+
+
+document.getElementById("surveyForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    showPopup();
+  });
+  
+  document.querySelector(".close").addEventListener("click", function() {
+    hidePopup();
+  });
+  
+  function showPopup() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  document.getElementById("searchButton").addEventListener("click", function() {
+    var searchInputValue = document.getElementById("searchInput").value;
+    // Here you can perform any action with the search input value,
+    // such as displaying search results or redirecting the user to a search results page
+    console.log("Search term:", searchInputValue);
+});document.getElementById("searchButton").addEventListener("click", function() {
+            var searchInputValue = document.getElementById("searchInput").value;
+            // Here you can perform any action with the search input value,
+            // such as displaying search results or redirecting the user to a search results page
+            console.log("Search term:", searchInputValue);
+        });
+  
